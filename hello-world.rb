6f28f9f363e66eeb54048883d22515bb8f543489
Randy Lai <randy.cs.lai@gmail.com>
2017-12-23 13:29:04 -0500

    add hello formula

---
+++ hello-world.rb
@@ -0,0 +1,20 @@
+class HelloWorld < Formula
+  desc "Program providing model for GNU coding standards and practices"
+  homepage "https://www.gnu.org/software/hello/"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz"
+  sha256 "31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b"
+
+  conflicts_with "camlistore", :because => "both install `hello` binaries"
+  conflicts_with "hello", :because => "both install `hello` binaries"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+  test do
+    assert_equal "brew", shell_output("#{bin}/hello --greeting=brew").chomp
+  end
+end
