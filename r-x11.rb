43f7bbc3248e5ca57b81cb74b66cbfa1015cadbf
Randy Lai <randy.cs.lai@gmail.com>
2018-03-05 14:24:10 -0500

    remove bottle

--- r-x11.rb
+++ r-x11.rb
@@ -3,13 +3,6 @@ class RX11 < Formula
   homepage "https://www.r-project.org/"
   url "https://cran.rstudio.com/src/base/R-3/R-3.4.3.tar.gz"
   sha256 "7a3cb831de5b4151e1f890113ed207527b7d4b16df9ec6b35e0964170007f426"
-  revision 1
-
-  bottle do
-    sha256 "49f0c4d16ae8ee11cbb06da9d2ed10371e3d1099117f72b0850ab3a47d88c514" => :high_sierra
-    sha256 "d4cb1268d6adeb1ee6be0e85b7f1f257277f3fd9666f3a9ab0e1b24b216ce2ab" => :sierra
-    sha256 "361c2934f40bc20fca7e0db3c25291095155a473c663f64a0b028f9dd7c0940d" => :el_capitan
-  end
 
   depends_on "pkg-config" => :build
   depends_on "gcc" # for gfortran
