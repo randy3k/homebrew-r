74bf290b3ce2f5a66aee540b4972f71304a1aeea
Randy Lai <randy.cs.lai@gmail.com>
2018-03-07 17:45:42 -0500

    boost-rstudio-server: revision 1

--- Formula/boost-rstudio-server.rb
+++ Formula/boost-rstudio-server.rb
@@ -4,6 +4,7 @@ class BoostRstudioServer < Formula
   url "https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.bz2"
   sha256 "beae2529f759f6b3bf3f4969a19c2e9d6f0c503edcb2de4a61d1428519fcb3b0"
   head "https://github.com/boostorg/boost.git"
+  revision 1
 
   bottle do
     root_url "https://github.com/randy3k/homebrew-r/releases/download/boost1.63-1.63.0"
